a7b79751aca12c29ce95961ecda56469937fb965
Lingye <84624193+WENSHAO521@users.noreply.github.com>
2026-07-04 19:05:43 +0900

    Update geo updater API usage

--- core/hub.go
+++ core/hub.go
@@ -347,34 +347,36 @@ func handleGetExternalProvider(externalProviderName string) string {
 	return string(data)
 }
 
-func handleUpdateGeoData(geoType string, geoName string, fn func(value string)) {
+func handleUpdateGeoData(geoType string, _ string, fn func(value string)) {
 	go func() {
-		path := constant.Path.Resolve(geoName)
 		switch geoType {
 		case "MMDB":
-			err := updater.UpdateMMDBWithPath(path)
+			err := updater.UpdateMMDB()
 			if err != nil {
 				fn(err.Error())
 				return
 			}
 		case "ASN":
-			err := updater.UpdateASNWithPath(path)
+			err := updater.UpdateASN()
 			if err != nil {
 				fn(err.Error())
 				return
 			}
 		case "GEOIP":
-			err := updater.UpdateGeoIpWithPath(path)
+			err := updater.UpdateGeoIp()
 			if err != nil {
 				fn(err.Error())
 				return
 			}
 		case "GEOSITE":
-			err := updater.UpdateGeoSiteWithPath(path)
+			err := updater.UpdateGeoSite()
 			if err != nil {
 				fn(err.Error())
 				return
 			}
+		default:
+			fn("unsupported geo data type: " + geoType)
+			return
 		}
 		fn("")
 	}()
